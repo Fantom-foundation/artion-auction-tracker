d74927235d13b3fb23927c70afe0e80b6531272f
jason <kwon_jason@yahoo.com>
2021-08-16 15:47:54 +1000

    fix

--- services/auctiontracker.js
+++ services/auctiontracker.js
@@ -22,6 +22,7 @@ const toLowerCase = (val) => {
   if (val) return val.toLowerCase()
   else return val
 }
+const decimalStore = new Map()
 const parseToken = async (inWei, paymentToken) => {
   paymentToken = toLowerCase(paymentToken)
   let tokenDecimals = decimalStore.get(paymentToken)
